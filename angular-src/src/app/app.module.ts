a7bed619f679105c6b59dc99abcc56dade53b45e
rachol <podaj_maslo@op.pl>
2017-03-25 12:18:19 +0200

    Finished MEAN Stack Front To Back part 6

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule, Routes} from '@angular/router'
+import { RouterModule, Routes} from '@angular/router';
+import { FlashMessagesModule} from 'angular2-flash-messages';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -12,13 +13,15 @@ import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
+import { ValidateService } from './services/validate.service';
+
 const appRoutes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'register', component: RegisterComponent},
     {path: 'login', component: LoginComponent},
     {path: 'dashboard', component: DashboardComponent},
     {path: 'profile', component: ProfileComponent}
-]
+];
 
 @NgModule({
   declarations: [
@@ -34,9 +37,10 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ ValidateService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
